8dc4b361b4c36ec9edfcfdcf4727c4a4618b9ac4
p313 <mala.ut.29@gmail.com>
2021-02-04 13:32:48 +0700

    feat: add routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.greeting}>
-          <div className={styles.about}></div>
+          <Link href="/about">
+            <div className={styles.about}></div>
+          </Link>
           <div className={styles.showcases}></div>
           <div className={styles.contact}></div>
         </h1>
