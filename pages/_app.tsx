73cd7f72c48cec23a56587b99ca0d1e1646b73a5
p313 <mala.ut.29@gmail.com>
2021-02-11 17:12:20 +0700

    styles: add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
 import type { AppProps } from 'next/app';
+import Link from 'next/link'
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <nav>
+        <Link href="/">home</Link>&nbsp;|&nbsp;
+        <Link href="/about">about</Link>&nbsp;|&nbsp;
+        <Link href="/showcase">showcase</Link>
+      </nav>
+      <Component {...pageProps} />
+      <footer>
+        <p>
+          <a href="https://github.com/athachai29"><img src="https://img.icons8.com/material-sharp/64/000000/github.png" /></a>&nbsp;
+          <a href="https://linkedin.com/in/athachai"><img src="https://img.icons8.com/material/64/000000/linkedin--v1.png" /></a>&nbsp;
+          <a href="athachai.m@gmail.com"><img src="https://img.icons8.com/material/64/000000/important-mail.png" /></a>
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default MyApp;
